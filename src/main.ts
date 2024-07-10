41ba9c0034c2fc14496272cd19fb7fd98bbf476d
kasashita <kasashita0124@gmail.com>
2024-07-10 21:20:31 +0900

    express環境構築

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { serve } from "https://deno.land/std@v0.39.0/http/server.ts";
+import express from "npm:express@4.19.2";
+import dotenv from "npm:dotenv@16.4.5";
+import process from "npm:process@0.11.10";
 
-const s = serve({ port: 1993 });
-console.log("access to http://localhost:1993/");
+dotenv.config();
 
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+const app = express();
+
+app.use("/", express.static(process.env.STATIC_PATH))
+
+// app.get("/", (req: any, res: any) => {
+//   res.send("welcome!!");
+// });
+
+app.listen(process.env.PORT);
